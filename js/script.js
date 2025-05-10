f4a33276209ead92eaa2d8d6226154392126ff74
Oliver Felix <oliverfelix.dev@gmail.com>
2025-05-10 03:40:32 +0100

    Header Added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,63 @@
-console.log("Hello, World!");
+let buttonHover = () => {
+  const createElm = function (menuItem) {
+    let menuItemsTexts = menuItem.children[0].children[0];
+
+    const menuItemsTextsArray = [...menuItemsTexts.textContent];
+
+    menuItemsTexts.textContent = "";
+
+    const textsArray = [];
+
+    menuItemsTextsArray.forEach((menuItemText) => {
+      textsArray.push(`<span>${menuItemText}</span>`);
+    });
+
+    textsArray.forEach((textArray) => {
+      menuItemsTexts.innerHTML += textArray;
+    });
+
+    const parentElm = menuItemsTexts.parentElement;
+
+    const parentElmHeight = parentElm.clientHeight;
+    parentElm.style.height = `${parentElmHeight}px`;
+
+    const cloneItem = menuItemsTexts.cloneNode(true);
+    parentElm.appendChild(cloneItem);
+  };
+
+  const animation = function (menuItem) {
+    gsap.defaults({
+      ease: "expo.out",
+      stagger: {
+        from: "11",
+        amount: 0.35,
+      },
+    });
+    menuItem.addEventListener("mouseover", function () {
+      gsap.to(this.children[0].children[0].children, {
+        y: "-100%",
+      });
+      gsap.to(this.children[0].children[1].children, {
+        y: "-100%",
+      });
+    });
+
+    menuItem.addEventListener("mouseleave", function () {
+      gsap.to(this.children[0].children[0].children, {
+        y: "0",
+      });
+      gsap.to(this.children[0].children[1].children, {
+        y: "0",
+      });
+    });
+  };
+
+  const targetItems = document.querySelectorAll(".btn-def");
+
+  targetItems.forEach((targetItem) => {
+    const menuItem = targetItem;
+    createElm(menuItem);
+    animation(menuItem);
+  });
+};
+buttonHover();
